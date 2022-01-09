69a3076912b5c2cc3991928d91d5fa064524fb79
Flamme <flamme.ariane@gmail.com>
2022-01-09 11:17:08 +0100

    add admin dashboard

--- app/Views/dashboardAdmin/index.php
+++ app/Views/dashboardAdmin/index.php
@@ -6,40 +6,72 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard utilisateur</title>
-    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+    <?php include 'assets/css.php'; ?>
 
 </head>
 
 <body>
 
     <div class="container">
+
         <div class="row">
 
             <h4> bienvenue sur le dashboard ADMIN </h4>
             <hr>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Nom</th>
-                        <th>Prenom</th>
-                        <th>Email</th>
-                        <th>Admin</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
 
-                        <td><?= $userInfo['firstname']; ?></td>
-                        <td><?= $userInfo['lastname']; ?></td>
-                        <td><?= $userInfo['email']; ?></td>
-                        <td><?= $userInfo['admin']; ?></td>
-                        <td><a href="<?= site_url('auth/logout'); ?>">Déconnexion</a></td>
-                    </tr>
-                </tbody>
-            </table>
+            <td><?= $userInfo['firstname']; ?></td>
+            <td><?= $userInfo['lastname']; ?></td>
+            <td><?= $userInfo['email']; ?></td>
+            <td><?= $userInfo['admin']; ?></td>
+
+            <td><a href="<?= site_url('auth/logout'); ?>">Déconnexion</a></td>
+        </div>
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12 mt-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>
+                            liste utilisateur
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Nom</th>
+                                    <th>Prénom</th>
+                                    <th>email</th>
+                                    <th>password</th>
+                                    <th>Date creation</th>
+                                    <th>statut</th>
+                                    <th>admin</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($listUsers as $listUsers) : ?>
+                                    <tr>
+                                        <td><?= $listUsers['firstname'] ?></td>
+                                        <td><?= $listUsers['lastname'] ?></td>
+                                        <td><?= $listUsers['email'] ?></td>
+                                        <td><?= $listUsers['password'] ?></td>
+                                        <td><?= $listUsers['creation_date'] ?></td>
+                                        <td><?= $listUsers['status'] ?></td>
+                                        <td><?= $listUsers['admin'] ?></td>
+                                        <td>
+                                            <a href="" class="btn btn-success btn-sm">Edit</a>
+                                            <a href="" class="btn btn-danger btn-sm">Delete</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
